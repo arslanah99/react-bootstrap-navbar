30a9a13b4a1cab4269f25af03b6c7c2e44b2aa9d
Diego Lorenesi <39860564+drlorenesi@users.noreply.github.com>
2020-05-03 09:55:12 -0600

    Update Navbar components.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactBootStrap from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import {
     BrowserRouter as Router,
     Link
